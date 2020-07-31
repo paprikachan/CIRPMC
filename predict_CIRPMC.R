4e3a2b13d9d9c10563edf76f520beb7db5b2d7a4
chenlingxi@ <chanlingxi@gmail.com>
2020-07-31 21:44:03 +0800

    update

--- predict_CIRPMC.R
+++ predict_CIRPMC.R
@@ -17,38 +17,28 @@ run <- function(infile, outfile){
   write.csv(pred_df, outfile, row.names = F)
 }
 
-pred_models <- function(model_list, X){ 
+pred_models <- function(model_list, X){
+  calibrate_model_list <- model_list$calibrate_model_list
   model_list <- model_list$model_list
-  LR_y_pred <- pred_func(model_list$LR, X)
-  SVM_y_pred <- pred_func(model_list$SVM, X)
-  GBDT_y_pred <- pred_func(model_list$GBDT, X)
-  KNN_y_pred <- pred_func(model_list$KNN, X)
-  NN_y_pred <- pred_func(model_list$NN, X)
-  df <- data.frame("LR"=LR_y_pred, "SVM"=SVM_y_pred, 
-                   "GBDT"=GBDT_y_pred, "KNN"=KNN_y_pred,
-                   'NN'=NN_y_pred)
+  df <- data.frame(row.names = rownames(X))
+  for (m in names(model_list)){
+    df[[m]] <- pred_func(model_list[[m]], calibrate_model_list[[m]], X)
+  }
   rownames(df) <- rownames(X)
-  colnames(df) <- c('LR', 'SVM', 'GBDT', 'KNN', 'NN')
   df$Probability <- df$SVM
-  rownames <- rownames(X[X$`TNF-a` > 0 & X$`TNF-a` < 8.1 
-                   & X$CRP > 0
-                   & X$`IL-2R` > 223 & X$`IL-2R` < 710
-                   & X$`IL-6` > 0 & X$`IL-6` < 7.0
-                   , ])
-  
-  df[rownames(df) %in% rownames, ]$Probability <- 0.01
-  
   df$Cluster <- as.factor(as.numeric(df$Probability >= 0.5))
   df$`Risk group`[df$Cluster == '0'] <- 'Non critical'
   df$`Risk group`[df$Cluster == '1'] <- 'Critical'
   return(df)
 }
 
-pred_func <- function(model, X){
-  pred_y <- predict(model, newdata = X, type='prob')[[2]]
-  return (pred_y)
-}
 
+pred_func <- function(model, calibrate_model, X){
+  pred_y <- predict(model, newdata = X, type='prob')[[2]]
+  tmp <- data.frame(X=pred_y)
+  calibrated_y <- predict(calibrate_model, newdata = tmp, type = "prob")[[2]]
+  return (calibrated_y)
+}
 
 library("optparse")
 # run CIRPMC (critical illness risk prediction model for COVID-19)
