53eee89c104bd47ce56c085670a0a9baada67fa6
chenlingxi@ <chanlingxi@gmail.com>
2020-09-06 12:10:53 +0800

    add server API

--- predict_MRPMC.R
+++ predict_MRPMC.R
@@ -12,12 +12,12 @@ run <- function(infile, outfile){
     X[[f]] <- as.numeric(X[[f]])
   }
 
-  pred_df <- pred_models(model_list, X[, fs])
+  pred_df <- pred_models(model_list, X[, fs], 0.6)
   
   write.csv(pred_df, outfile, row.names = F)
 }
 
-pred_models <- function(model_list, X){
+pred_models <- function(model_list, X, cutoff){
   calibrate_model_list <- model_list$calibrate_model_list
   model_list <- model_list$model_list
   df <- data.frame(row.names = rownames(X))
@@ -25,8 +25,8 @@ pred_models <- function(model_list, X){
     df[[m]] <- pred_func(model_list[[m]], calibrate_model_list[[m]], X)
   }
   rownames(df) <- rownames(X)
-  df$Probability <- df$SVM
-  df$Cluster <- as.factor(as.numeric(df$Probability >= 0.5))
+  df$Ensemble <- df$LR*0.25 + df$SVM*0.3 + + df$GBDT*0.1 + df$NN*0.35
+  df$Cluster <- as.factor(as.numeric(df$Ensemble >= cutoff))
   df$`Risk group`[df$Cluster == '0'] <- 'Non critical'
   df$`Risk group`[df$Cluster == '1'] <- 'Critical'
   return(df)
@@ -41,7 +41,7 @@ pred_func <- function(model, calibrate_model, X){
 }
 
 library("optparse")
-# run CIRPMC (critical illness risk prediction model for COVID-19)
+# run MRPMC (Mortality Risk Prediction Model for COVID-19)
 option_list = list(
   make_option(c("-i", "--infile"), type="character", default=NULL, 
               help="Path of X input file", metavar="character"),
